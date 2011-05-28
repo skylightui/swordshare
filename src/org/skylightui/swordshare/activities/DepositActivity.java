6aba52dec661e694c8a69f9750ce20502010ee22
Skylight <stuart@stuartlewis.com>
2011-05-28 16:33:32 +1200

    New code to collect basic metadata

--- src/org/skylightui/swordshare/activities/DepositActivity.java
+++ src/org/skylightui/swordshare/activities/DepositActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import org.skylightui.swordshare.R;
 import org.skylightui.swordshare.util.SimpleSWORDDeposit;
 import org.skylightui.swordshare.util.StackTraceLogger;
@@ -15,69 +17,82 @@ import org.skylightui.swordshare.util.StackTraceLogger;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Hashtable;
 
-public class DepositActivity extends Activity
-{
+public class DepositActivity extends Activity {
+
+    TextView title;
+    TextView description;
+    Intent i;
+
     /** The debugging tag */
     private static final String TAG = "org.skylightui.swordshare.activities.DepositActivity";
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.describe);
+        String filename = "unknown";
+        Uri uri = null;
 
         // The Intent that started this activity
-        Intent i = this.getIntent();
-
-        // The URL of the deposit, if applicable
-        String url = "unknown";
+        i = this.getIntent();
 
         // Is the Intent an action being sent from somewhere else?
         if ((i.getAction() != null) && (i.getAction().equals(Intent.ACTION_SEND))) {
             try {
                 Context context = getApplicationContext();
                 // Retrieve the Uri of the file being referenced
-                Uri uri = (Uri)i.getExtras().get(Intent.EXTRA_STREAM);
-                String filename = uri.toString().substring(uri.toString().indexOf(':') + 1);
+                uri = (Uri)i.getExtras().get(Intent.EXTRA_STREAM);
 
-                CharSequence text = "Data: " + i.getType() + " from " + filename;
-                int duration = Toast.LENGTH_LONG;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-
-                FileOutputStream fosmets = openFileOutput("mets.xml", Context.MODE_PRIVATE);
-
-                Hashtable<String, String> metadata = new Hashtable<String, String>();
-                metadata.put("creator", "Lewis, Stuart");
-                metadata.put("title", "Test title");
-                metadata.put("description", "Test description");
-
-                Log.d(TAG, "About to initiate deposit");
-                SimpleSWORDDeposit deposit = new SimpleSWORDDeposit(filename, i.getType(), metadata, fosmets);
-
-                InputStream content = context.getContentResolver().openInputStream(uri);
-
-                Log.d(TAG, "About to call makePackage");
-                FileOutputStream foszip = openFileOutput("package.zip", Context.MODE_PRIVATE);
-                FileInputStream fismets = openFileInput("mets.xml");
-                deposit.makePackage(content, uri.toString(), foszip, fismets);
-
-                Log.d(TAG, "About to call deposit");
-                FileInputStream fispackage = openFileInput("package.zip");
-                deposit.deposit(fispackage, "http://192.168.2.247:8080/sword/deposit/123456789/766", "sword@swordapp.org", "sword");
-                url = deposit.getURL();
-                Log.d(TAG, "identifier = " + url);
             } catch (Exception e) {
                 StackTraceLogger.getStackTraceString(e, TAG);
             }
         }
 
-        setContentView(R.layout.deposit);
-        TextView turl = (TextView)findViewById(R.id.url);
-        turl.setText("URL: " + url);
+        ImageView image = (ImageView)findViewById(R.id.image);
+        image.setImageURI(uri);
+
+        title = (TextView)this.findViewById(R.id.title);
+        description = (TextView)this.findViewById(R.id.description);
+
+        Button button = (Button)this.findViewById(R.id.depositbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Context context = getApplicationContext();
+                    // Retrieve the Uri of the file being referenced
+                    Uri uri = (Uri)i.getExtras().get(Intent.EXTRA_STREAM);
+                    String filename = uri.toString().substring(uri.toString().indexOf(':') + 1);
+
+                    FileOutputStream fosmets = openFileOutput("mets.xml", Context.MODE_PRIVATE);
+
+                    Hashtable<String, String> metadata = new Hashtable<String, String>();
+                    metadata.put("creator", "Lewis, Stuart");
+                    metadata.put("title", title.getText().toString());
+                    metadata.put("description", description.getText().toString());
+
+                    Log.d(TAG, "About to initiate deposit");
+                    SimpleSWORDDeposit deposit = new SimpleSWORDDeposit(filename, i.getType(), metadata, fosmets);
+
+                    InputStream content = context.getContentResolver().openInputStream(uri);
+
+                    Log.d(TAG, "About to call makePackage");
+                    FileOutputStream foszip = openFileOutput("package.zip", Context.MODE_PRIVATE);
+                    FileInputStream fismets = openFileInput("mets.xml");
+                    deposit.makePackage(content, uri.toString(), foszip, fismets);
+
+                    Log.d(TAG, "About to call deposit");
+                    FileInputStream fispackage = openFileInput("package.zip");
+                    deposit.deposit(fispackage, "http://192.168.2.247:8080/sword/deposit/123456789/766", "sword@swordapp.org", "sword");
+                    String url = deposit.getURL();
+                    Log.d(TAG, "identifier = " + url);
+                    setContentView(R.layout.deposit);
+                    TextView turl = (TextView)findViewById(R.id.url);
+                    turl.setText("URL: " + url);
+                } catch (Exception e) {
+                    StackTraceLogger.getStackTraceString(e, TAG);
+                }
+            }
+        });
     }
 }
